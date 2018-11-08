718c602753428baeb5e5b2b6464ccc228e30de80
Jaap Roes <jroes@leukeleu.nl>
2018-11-08 14:35:32 +0100

    Tweak middleware, prevent overwriting existing `Link` header

--- push_redirect/middleware.py
+++ push_redirect/middleware.py
@@ -2,12 +2,22 @@ from django.utils.http import is_safe_url
 
 
 class Http2ServerPushRedirectMiddleware:
+    redirect_status_codes = {301, 302}
+
     def __init__(self, get_response):
         self.get_response = get_response
 
+    def should_preload(self, request, response):
+        return (
+            request.is_secure
+            and response.status_code in self.redirect_status_codes
+            and hasattr(response, 'url')
+            and not response.has_header('Link')
+        )
+
     def __call__(self, request):
         response = self.get_response(request)
-        if request.is_secure and response.status_code in {301, 302} and hasattr(response, 'url'):
+        if self.should_preload(request, response):
             url = response.url
             if is_safe_url(url, allowed_hosts={request.get_host()}, require_https=True):
                 response['Link'] = f'<{url}>; rel=preload'
