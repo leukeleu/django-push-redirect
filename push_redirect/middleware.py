2a0decc18645d0c8a0eb524eceae02d4692d12fb
Jaap Roes <jroes@leukeleu.nl>
2019-12-02 15:46:45 +0100

    Refactor Http2ServerPushRedirectMiddleware

--- push_redirect/middleware.py
+++ push_redirect/middleware.py
@@ -1,24 +1,59 @@
-from django.utils.http import is_safe_url
+from django.conf import settings
+from django.core.exceptions import MiddlewareNotUsed
+from django.utils.http import url_has_allowed_host_and_scheme
 
 
 class Http2ServerPushRedirectMiddleware:
-    redirect_status_codes = {301, 302}
+    redirect_status_codes = {301, 302, 303, 307, 308}
 
     def __init__(self, get_response):
+        if not settings.SECURE_PROXY_SSL_HEADER:
+            # For Django to be able to detect if a request is secure
+            # some configuration is required, e.g..
+            #
+            # The nginx proxy should set:
+            #
+            #   proxy_set_header X-Forwarded-Proto $scheme;
+            #
+            # The Django project should set:
+            #
+            #   SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+            raise MiddlewareNotUsed('SECURE_PROXY_SSL_HEADER is not configured')
         self.get_response = get_response
 
     def should_preload(self, request, response):
-        return (
-            request.is_secure
-            and response.status_code in self.redirect_status_codes
-            and hasattr(response, 'url')
-            and not response.has_header('Link')
-        )
+        if not request.is_secure:
+            # HTTP/2 requires SSL/TLS.
+            return False
+
+        if not request.method == 'GET':
+            # Err on the side of caution by only preloading GET requests
+            return False
+
+        if response.status_code not in self.redirect_status_codes:
+            # Not a redirect
+            return False
+
+        if not hasattr(response, 'url'):
+            # The url attribute is added by the HttpResponseRedirectBase
+            # base class. This response does not have it so ignore it.
+            return False
+
+        if response.has_header('Link'):
+            # There already is a Link header, don't overwrite it
+            return False
+
+        # Preload is enabled if the redirect url is on the same host
+        # as the request and using the same protocol (https)
+        return url_has_allowed_host_and_scheme(
+            response.url,
+            allowed_hosts={request.get_host()},
+            require_https=True)
 
     def __call__(self, request):
         response = self.get_response(request)
         if self.should_preload(request, response):
+            # Response can be preloaded, add Link header
             url = response.url
-            if is_safe_url(url, allowed_hosts={request.get_host()}, require_https=True):
-                response['Link'] = f'<{url}>; rel=preload'
+            response['Link'] = f'<{url}>; as=document; rel=preload'
         return response
